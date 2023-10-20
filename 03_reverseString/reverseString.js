a0ec5474fed922bab1eb8f57a0eac086020a1504
eramaan <58112194+eramaan@users.noreply.github.com>
2023-10-20 18:50:02 +0200

    give me the green light

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordReversed = '';
+    let wordLettersCount = word.length;
+    for (i = wordLettersCount; i > -1; i--) {
+        wordReversed = wordReversed + word.charAt(i);
+    }
+    return wordReversed;
 };
+
+//their solution
+// const reverseString = function (string) {
+//     return string.split("").reverse().join("");
+//   };
+// creates an array with every single letter as different element, then invert the order, then joins all them together again  
+  
 
 // Do not edit below this line
 module.exports = reverseString;

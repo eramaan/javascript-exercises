342fb0c305e432ac2e490e2917cfd09fe4ace3ef
eramaan <58112194+eramaan@users.noreply.github.com>
2023-10-20 19:59:53 +0200

    green light again, but this one has been hard

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...toBeRemovedArray) {
+    let removed = []
+    for (let i = 0; i < toBeRemovedArray.length; i++) {   //cicla elementi dell'array da rimuovere 
+    
+        for (let j = 0; j < myArray.length; j++) { //cicla elementi dell'array di partenza
+            if (toBeRemovedArray[i] === myArray[j]) {
+                removed = myArray.splice(j,1)
+                console.log("removed: " + removed);
+                console.log("what's left: " + myArray)
+            } else {
+            }
+        }
+    }
+return myArray;
 };
 
 // Do not edit below this line

d169aa6b797b4523e72f9bd6180d48671b3734e8
eramaan <58112194+eramaan@users.noreply.github.com>
2023-10-20 18:25:14 +0200

    done and green on all the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i = 0
+    let answer = ''
+    if(num < 0) {
+        answer = "ERROR";
+        return answer;
+    } else {
+        for (i = 0; i < num; i++) {
+            answer = answer + string
+        }
+        return answer;
+        }
+    };
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;

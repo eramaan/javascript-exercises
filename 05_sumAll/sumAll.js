e9ca1882ac74ec784dd975f3050877d286fc543b
eramaan <58112194+eramaan@users.noreply.github.com>
2023-10-20 20:47:43 +0200

    green light, and lemme tell u that write comments and then explain it to your computer with the code make so much more sense

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,35 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    // check if positive integer
+    if (
+        numA <= 0 || 
+        numB <= 0 || 
+        Number.isInteger(numA) == false || 
+        Number.isInteger(numB) == false
+        ) {
+        // if no, throw an error
+        return "ERROR";
+        // if yes, go on
+    } else { 
+        let min = 0;
+        let max = 0;
+        let sum = 0;
+        // check which one is the bigger number
+        // assign bigger number as the max, the other one as min
+        if (numB > numA) {
+            min = numA;
+            max = numB;
+        } else {
+            min = numB;
+            max = numA;
+        }
+        // cycle every number and add it to the sum
+        for (i = min; i <= max; i++) {
+            sum = sum + i;
+        }
+       
+        return sum
+    }
+
 
 };
 

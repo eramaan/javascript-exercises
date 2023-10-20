0bad94ea50c55b36e65e2ab831193a23a99a3cf4
eramaan <58112194+eramaan@users.noreply.github.com>
2023-10-20 20:03:40 +0200

    added TOP solution, and gosh how easy it looks

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -14,5 +14,23 @@ const removeFromArray = function(myArray, ...toBeRemovedArray) {
 return myArray;
 };
 
+// aaand again their solution is way easier than mine, next time should really look for some other ways to do stuff than using for loops 
+// const removeFromArray = function (array, ...args) {
+//     // create a new empty array
+//     const newArray = [];
+//     // use forEach to go through the array
+//     array.forEach((item) => {
+//       // push every element into the new array
+//       // UNLESS it is included in the function arguments
+//       // so we create a new array with every item, except those that should be removed
+//       if (!args.includes(item)) {
+//         newArray.push(item);
+//       }
+//     });
+//     // and return that array
+//     return newArray;
+//   };
+
+
 // Do not edit below this line
 module.exports = removeFromArray;

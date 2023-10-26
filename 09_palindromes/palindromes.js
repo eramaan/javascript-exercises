e5ca4035f8078298eddacd0aa246182a2380951d
eramaan <58112194+eramaan@users.noreply.github.com>
2023-10-26 17:30:30 +0200

    aaand all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (text) {
 
+    let newText = ""
+
+    // edit string to remove punctuation, spaces and uppercase
+    function removePunctuation(text) {
+        let punctuation = /[\.,?! ]/g;
+        newText = text.replace(punctuation, "").toLowerCase();
+   }
+    
+    removePunctuation(text)
+
+    let newTextReversed = newText.split("").reverse().join("");
+
+    if (newText === newTextReversed) {
+        return true
+    } else {
+        return false
+    }
+    
 };
 
 // Do not edit below this line
